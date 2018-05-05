c8cd249f9f10108b17f73574861ac47cb9cf53ed
Amit Gupta <amitgupta.gwl@gmail.com>
2018-05-05 12:20:02 +0530

    add multiple groups

---
+++ tags/galleries.tag
@@ -0,0 +1,6 @@
+<galleries>
+    <gallery each={ this.repeat }></gallery>
+    <script>
+        this.repeat = new Array(Number.parseInt(this.opts.count));
+    </script>
+</galleries>
--- tags/tags.js
+++ tags/tags.js
@@ -1,3 +1,6 @@
+riot.tag2('galleries', '<gallery each="{this.repeat}"></gallery>', '', '', function(opts) {
+        this.repeat = new Array(Number.parseInt(this.opts.count));
+});
 riot.tag2('gallery', '<label class="btn-bs-file btn btn-outline-info">Browse Image files <input type="file" class="filebutton" accept="image/*" onchange="{readImageFiles}" multiple> </label> <div class="input-bar clearfix"> <div class="left-paddle"></div> <div class="photolist-wrapper"> <div class="photolist"> <img riot-src="{src}" label="{name}" title="{name}" width="80px" each="{galleryData}"> </div> </div> <div class="right-paddle"></div> </div>', '', '', function(opts) {
         this.readImageFiles = function(e) {
             var input = e.srcElement;
