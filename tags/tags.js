2797dea871f40a69aebd7d2d38d1d1e6b7bde8d5
Amit Gupta <amitgupta.gwl@gmail.com>
2018-05-28 07:39:55 +0530

    compile riot tags

--- tags/tags.js
+++ tags/tags.js
@@ -1,4 +1,4 @@
-riot.tag2('decktemplate', '<div id="action-bar" class="row align-items-center"> <div class="col form-inline"> <i class="fa fa-repeat action-btn btn btn-info" title="Arrange Randomly" onclick="{rotateRandomly}"></i> <div class="input-group"> <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="resize-action" checked> <label class="form-check-label" for="resize-action"> Maintain height-width ratio </label> </div> <span class="fa-stack fa-lg action-btn btn btn-info" title="Resize Randomly" style="font-size: 1.2em;" onclick="{resizeRandomly}"> <i class="fa fa-square-o fa-stack-2x" style="top: -0.5px;"></i> <i class="fa fa-arrows-h fa-stack-1x" style="top: -0.5px;"></i> </span> </div> <i class="fa fa-random action-btn btn btn-info" title="Arrange Randomly" onclick="{arrangeRandomly}"></i> <i class="fa fa-copy action-btn btn btn-info" title="Copy Pattern" onclick="{copy}"></i> <i class="fa fa-paste action-btn btn btn-info" title="Paste Pattern" onclick="{paste}"></i> <label class="btn-bs-file"> <i class="fa fa-folder-open-o action-btn btn btn-info" title="Open Pattern file" onclick="{this.parent.arrangeRandomly}"></i> <input type="file" class="filebutton" accept="application/vnd.nimn,*.nmn,*.nimn" onchange="{readTemplateFile}"> </label> <div class="form-inline input-group"> <input id="exportTemplateName" type="text" class="form-control" placeholder="Enter the template name " riot-value="{exportTemplateName}" style="width: 300px;"> <i class="fa fa-save action-btn btn btn-info" title="Save Pattern to external file" onclick="{exportTemplate}"></i> </div> </div> </div> </div>', '', '', function(opts) {
+riot.tag2('decktemplate', '<div id="action-bar" class="row align-items-center"> <div class="col form-inline"> <i class="fa fa-repeat action-btn btn btn-info" title="Arrange Randomly" onclick="{rotateRandomly}"></i> <div class="input-group"> <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="resize-action" checked> <label class="form-check-label" for="resize-action"> Maintain height-width ratio </label> </div> <span class="fa-stack fa-lg action-btn btn btn-info" title="Resize Randomly" style="font-size: 1.2em;" onclick="{resizeRandomly}"> <i class="fa fa-square-o fa-stack-2x" style="top: -0.5px;"></i> <i class="fa fa-arrows-h fa-stack-1x" style="top: -0.5px;"></i> </span> </div> <i class="fa fa-random action-btn btn btn-info" title="Arrange Randomly" onclick="{arrangeRandomly}"></i> <i class="fa fa-copy action-btn btn btn-info" title="Copy Pattern" onclick="{copy}"></i> <i class="fa fa-paste action-btn btn btn-info" title="Paste Pattern" onclick="{paste}"></i> <label class="btn-bs-file"> <i class="fa fa-folder-open-o action-btn btn btn-info" title="Open Pattern file" onclick="{this.parent.arrangeRandomly}"></i> <input id="file-input" type="file" class="filebutton" accept="application/vnd.nimn,*.nmn,*.nimn" onchange="{readTemplateFile}"> </label> <div class="form-inline input-group"> <input id="exportTemplateName" type="text" class="form-control" placeholder="Enter the template name " riot-value="{exportTemplateName}" style="width: 300px;"> <i class="fa fa-save action-btn btn btn-info" title="Save Pattern to external file" onclick="{exportTemplate}"></i> </div> </div> </div> </div>', '', '', function(opts) {
 
         this.selectCards = function(cb,...arg){
             var elArr = $(".cf-selected");
@@ -218,7 +218,40 @@ riot.tag2('decktemplate', '<div id="action-bar" class="row align-items-center"> 
 
             setTimeout(function(){ $("#snackbar").removeClass("show"); $("#snackbar").text("");}, 3000);
         }
+
+        this.on("mount",() => {
+          window.addEventListener("keypress", (press) => {
+            switch(press.key) {
+              case "r":
+                this.selectCards(rotateSymbolsRandomly);
+                break;
+              case "R":
+                this.resizeRandomly();
+                break;
+              case "a":
+                this.selectCards(setRandomPos);
+                break;
+              case "c":
+                this.copy(e);
+                break;
+              case "C":
+                this.copy(e)
+                break;
+              case "v":
+                this.paste();
+                break;
+              case "o":
+                $('#file-input').trigger('click');
+                break;
+              case "s":
+                this.exportTemplate(e);
+                break;
+            }
+          });
+        });
+
 });
+
 riot.tag2('design', '<div class="row"> <div class="col-md-4"> <select id="cardsize" class="form-control" onchange="{changeDemoCardSize}"> <option disabled="true">Select Size</option> <option each="{cardsize,name in cards}" riot-value="{name}" selected="{name == \'Normal Playing Card Or Bridge Size\'}">{name}</option> </select> <div class="empty"></div> <select id="symbolscount" onchange="{checkSymbolCount}" class="form-control"> <option selected="false" disabled="true">Number of symbols on a card</option> <option>2</option> <option>3</option> <option>4</option> <option>5</option> <option>6</option> <option>7</option> <option>8</option> <option>9</option> <option>10</option> </select> <div class="empty"></div> <div>Choose background color</div> <input id="colorpicker" onchange="{changeBgColor}" value="#ffffff" style="width:100%;" type="color"> <div class="empty"></div> <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="rotate" checked="true"> <label class="form-check-label" for="rotate"> Rotate randomly </label> </div> <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="resize" checked="true"> <label class="form-check-label" for="resize"> Resize randmly </label> </div> <div class="form-check"> <input class="form-check-input" type="checkbox" value="" id="maintainratio" checked="true"> <label class="form-check-label" for="maintainratio"> Maintain height width ratio </label> </div> </div> <div class="col-md-8"> <div id="slider-horizontal-val" class="text-center" style="width:160mm;"></div> <div id="slider-horizontal"></div> <div id="demo-card-container"> <div style="float:left; width:160mm; height:160mm;"> <div id="demo-card"> </div> </div> <div id="slider-vertical" style="float:left"></div> <div id="slider-vertical-val" style="float:left"></div> </div> </div> </div>', 'design .ui-slider .ui-slider-handle,[data-is="design"] .ui-slider .ui-slider-handle{ width: 0.8em; height: 0.8em; } design #slider-vertical,[data-is="design"] #slider-vertical{ height: 150mm; width: 5px; } design #slider-horizontal,[data-is="design"] #slider-horizontal{ width: 150mm; height: 5px; } design #demo-card,[data-is="design"] #demo-card{ display: block; outline: 1px solid grey; margin-top: 10px; } design #demo-card-container,[data-is="design"] #demo-card-container{ height: 160mm; } design #slider-vertical-val,[data-is="design"] #slider-vertical-val{ writing-mode: tb-rl; height: 100%; text-align: center; }', '', function(opts) {
 
         this.cards = {
