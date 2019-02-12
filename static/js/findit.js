dfe264918919ec60e22a69cf93e139ef5bf2a0f8
Amit Gupta <amitgupta.gwl@gmail.com>
2019-02-12 18:39:42 +0530

    fix: selection of symbols on the card
    symbols on the card should be n + 1 where n is a prime number

--- static/js/findit.js
+++ static/js/findit.js
@@ -40,5 +40,65 @@ function totalCombinations(n){
     return Math.pow(p,2) + p + 1;
 }
 
-/* console.log(createBlocks(4));
-console.log(totalCombinations(4)); */
+function createBlocksForNonPrime(size){
+    var blocks = [];
+    var matrix = new Array([7]);
+
+
+}
+
+//console.log(createBlocks(9));
+//console.log(totalCombinations(9));
+
+//test
+
+const SymbolsOnACard = 6; // n = 2, 3, 4, 6, 8, 12, 14, 18, 20, 24, 30, 32; n-1 is a prime number
+const blocks = createBlocks( SymbolsOnACard );
+//console.log(totalCombinations( SymbolsOnACard ));
+
+function checkForCardWithoutCommonSymbol(){
+
+    console.log("Looking for cards with no common symbol");
+    //let unmatchingCards = new Set();
+    const unmatchingCards = {};
+    
+    let counter = 0;
+    for(let i=0;i<blocks.length; i++){
+        let unmatchingCounter = 0;
+        let matchingCounter = 0;
+        for(let j=0;j<blocks.length; j++){
+            if(i===j) continue;
+            if( doesIntersect( blocks[i], blocks[j]) ) {
+                matchingCounter++;
+            }else{
+                //console.log(`${blocks[j]} is not matching with ${blocks[i]}`)
+                unmatchingCounter++;
+                counter++;
+                //unmatchingCards.push({I:i, card: blocks[j]});
+                if( !unmatchingCards[i] ) unmatchingCards[i] = [ blocks[i] ];
+                //unmatchingCards[i].push({index: j, cards: blocks[j] });
+                //unmatchingCards[i].push(blocks[j]);
+                unmatchingCards[i].push(j);
+            }
+        }
+    
+        if(matchingCounter === (blocks.length -1) ){
+            //console.log("Complete match", i, blocks[i]);
+        }
+    }
+    console.log("Unmatching Cards:" , unmatchingCards);
+    console.log("Total Unmatching Cards:" , Object.keys(unmatchingCards).length );
+    if( Object.keys(unmatchingCards).length > 0){
+        console.log("Each card is unmatching with" , unmatchingCards[ Object.keys(unmatchingCards)[0] ].length - 1,  " cards" );
+    }
+    //console.log("Unmatching Cards:" , JSON.stringify(unmatchingCards, null,1 ) );
+}
+
+function doesIntersect(arr1, arr2){
+    for(let i=0;i<arr1.length; i++){
+        if(arr2.indexOf(arr1[i]) !== -1) return true;
+    }
+    return false;
+}
+
+//checkForCardWithoutCommonSymbol();
